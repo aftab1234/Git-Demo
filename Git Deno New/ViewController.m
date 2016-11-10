b13263deab715a158fc3c4f4961c8c75f714ad83
Mohammad Aftab Sabir <maftabsabir@gmail.com>
2016-11-10 12:47:05 +0530

    commit changes in new branch

--- Git Deno New/ViewController.m
+++ Git Deno New/ViewController.m
@@ -19,6 +19,12 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+
+-(void)sayHello{
+    NSLog(@"Hello");
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
